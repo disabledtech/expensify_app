cd2374476daa0b79aa8560bdb8b5df999389793e
Taylor Danielson <disabledtech@gmail.com>
2019-06-04 14:41:08 -0700

    Added private routes for logged in users

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Router, Route, Switch} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import AddExpense from "../components/AddExpense";
 import EditExpense from "../components/EditExpense";
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import Header from "../components/Header";
 import Help from "../components/Help";
 import NotFound from "../components/NotFound";
 import Login from '../components/Login';
+import PrivateRoute from './PrivateRoute';
 
+export const history = createHistory()
 
 const AppRouter = () => (
-    <BrowserRouter>
-            <div>
-                <Header />
-                <Switch>
-                    <Route path="/" component={Login} exact={true} />
-                    <Route path="/dashboard" component={ExpenseDashboardPage} />
-                    <Route path="/create" component={AddExpense} />
-                    <Route path="/edit/:id" component={EditExpense} />
-                    <Route path="/help" component={Help} />
-                    <Route component={NotFound} />
-                </Switch>    
-            </div>         
-        </BrowserRouter>
+    <Router history={history}>
+        <div>
+            <Switch>
+                <Route path="/" component={Login} exact={true} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpense} />
+                <PrivateRoute path="/edit/:id" component={EditExpense} />
+                <Route path="/help" component={Help} />
+                <Route component={NotFound} />
+            </Switch>    
+        </div>         
+    </Router>
 )
 
 export default AppRouter;
