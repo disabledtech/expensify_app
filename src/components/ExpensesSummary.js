3fc808e70f31533a16570a0351d100f868716b40
Taylor Danielson <disabledtech@gmail.com>
2019-06-10 15:11:35 -0700

    Styles for header and summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,16 +3,20 @@ import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 
 export const ExpensesSummary = ({ expenseCount, expenseTotal }) => {
     const pluralExpenseOrNot = expenseCount === 1 ? 'expense': 'expenses';
     const total = numeral(expenseTotal / 100).format('$0,0.00');
 
     return (
-        <div>
-            {   
-                <h2>Viewing a total of { expenseCount } { pluralExpenseOrNot } for { total }.</h2>   
-            }
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing a total of <span>{ expenseCount }</span> { pluralExpenseOrNot } for <span>{ total }</span>.</h1>   
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>          
         </div>
     );
 };
